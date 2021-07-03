080fac0592d67e68e4ee50d067f5a86ad3e71d7e
yuikaueda <yuika428@gmail.com>
2021-07-03 18:12:59 +0900

    0703

--- sd_t.py
+++ sd_t.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data1 = np.loadtxt('n10_r0rt100000.dat')
-data2 = np.loadtxt('n10000_r0rt100000.dat')
+data1 = np.loadtxt('nor_n10_r0rt1000000.dat')
+data2 = np.loadtxt('nor_n1000_r0rt1000000.dat')
 
 x1 = data1[:,0]
 y1 = data1[:,1]
@@ -16,8 +16,10 @@ ax.plot(x1, y1, 'o', markersize=1, c='black', label = r'$N=10$')
 ax.plot(x2, y2, 'o', markersize=1, c='red', label = r'$N=1000$')
 
 plt.xlabel("Time", fontsize = 18)
-plt.xlabel("S.D.", fontsize = 18)
+plt.ylabel("S.D.", fontsize = 18)
+
+plt.xlim(0,1)
 
 ax.legend(loc='best')
-fig.savefig("n10n10000_r100000.png")
+fig.savefig("nor_n10n1000_r1000000.png")
 plt.show()
