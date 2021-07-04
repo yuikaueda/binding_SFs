93b992e1face15517f588596ac04a068edc32ace
yuikaueda <yuika428@gmail.com>
2021-07-04 18:17:06 +0900

    0704n

--- sd_t.py
+++ sd_t.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data1 = np.loadtxt('nor_n1000_r0rt100.dat')
-data2 = np.loadtxt('nor_n1000_r0rt1000.dat')
-data3 = np.loadtxt('nor_n1000_r0rt10000.dat')
-data4 = np.loadtxt('nor_n1000_r0rt100000.dat')
+data1 = np.loadtxt('nor_n10_r0rt100000.dat')
+data2 = np.loadtxt('nor_n100_r0rt100000.dat')
+data3 = np.loadtxt('nor_n1000_r0rt100000.dat')
+data4 = np.loadtxt('nor_n10000_r0rt100000.dat')
 
 x1 = data1[:,0]
 y1 = data1[:,1]
@@ -20,10 +20,10 @@ y4 = data4[:,1]
 
 fig, ax = plt.subplots(1, 1)
 
-ax.plot(x1, y1, 'o', markersize=1, c='black', label = r'$r=100$')
-ax.plot(x2, y2, 'o', markersize=1, c='red', label = r'$r=1000$')
-ax.plot(x3, y3, 'o', markersize=1, c='blue', label = r'$r=10000$')
-ax.plot(x4, y4, 'o', markersize=1, c='green', label = r'$r=100000$')
+ax.plot(x1, y1, 'o', markersize=1, c='black', label = r'$N=10$')
+ax.plot(x2, y2, 'o', markersize=1, c='red', label = r'$N=100$')
+ax.plot(x3, y3, 'o', markersize=1, c='blue', label = r'$N=1000$')
+ax.plot(x4, y4, 'o', markersize=1, c='green', label = r'$N=10000$')
 
 plt.xlabel("Time", fontsize = 18)
 plt.ylabel("S.D.", fontsize = 18)
@@ -31,5 +31,5 @@ plt.ylabel("S.D.", fontsize = 18)
 #plt.xlim(0,1)
 
 ax.legend(loc='best')
-fig.savefig("tmax_nor_n1000_r_all.png")
+fig.savefig("tmax_nor_n_all_r100000.png")
 plt.show()
